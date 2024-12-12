c032ce166965ab52c54131f7a2784dc8e3960592
john.reynolds <john.reynolds@MU-C1-03.mullen.pvt.k12.co.us>
2024-12-12 13:04:08 -0700

    In Class Commit 1 12/12
    -Fixed issue of scaleChoice going instead of digChoice
    -Project complete

--- Explorer Game with Graphical Inputs/src/JPanel.java
+++ Explorer Game with Graphical Inputs/src/JPanel.java
@@ -27,7 +27,7 @@ public class JPanel extends JFrame
 				// Start Game Button
 
 				Object[] options =
-				{ "START" };
+					{ "START" };
 				startGame = JOptionPane.showOptionDialog(frame, "Press START to start the game", "Your Decision",
 						JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
 
@@ -37,138 +37,123 @@ public class JPanel extends JFrame
 			{
 				// Drop down options
 				final String[] castleChoice =
-				{ "Scale the wall", "Dig under the wall" };
+					{ "Scale the wall", "Dig under the wall" };
 				final JFrame frame = new JFrame();
 				adventureType = (String) JOptionPane.showInputDialog(frame,
 						"You walk up to a castle. You walk up the wall. Do you...", "Adventure Genre",
-						JOptionPane.QUESTION_MESSAGE, null, castleChoice, -1);
+						JOptionPane.QUESTION_MESSAGE, null, castleChoice, null);
 
 			}
 
 		public static void secondBranch()
 			{
-				switch (adventureType)
-				{
+				if (adventureType.equals("Scale the wall"))
+					{
 
-				// Scale the Wall
+						// Scale the Wall
+						
+						JOptionPane.showMessageDialog(frame, "You scale the wall");
 
-				case "Scale the wall":
-				{
-					JOptionPane.showMessageDialog(frame, "You scale the wall");
+						Object[] options =
+							{ "Stairs", "Rope" };
+						scaleChoice = JOptionPane.showOptionDialog(frame,
+								"You see a rope going down into the castle or a staircase leading down one of the towers. Which one do you go down?",
+								"Your Choice", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null,
+								options, null);
 
-					Object[] options =
-					{ "Stairs", "Rope" };
-					scaleChoice = JOptionPane.showOptionDialog(frame,
-							"You see a rope going down into the castle or a staircase leading down one of the towers. Which one do you go down?",
-							"Your Choice", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null,
-							options, -1);
+					}
 
-					break;
-				}
+				else if (adventureType.equals("Dig under the wall"))
+					{
 
-				// Dig under the wall
+						// Dig under the wall
 
-				case "Dig under the wall":
-				{
-					JOptionPane.showMessageDialog(frame, "You dig under the wall");
+						JOptionPane.showMessageDialog(frame, "You dig under the wall");
 
-					Object[] options =
-					{ "Stone", "Dirt" };
-					digChoice = JOptionPane.showOptionDialog(frame,
-							"You approach a wall of stone and a wall of dirt. Which one do you did through?",
-							"Your Choice", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null,
-							options, -1);
+						Object[] options1 =
+							{ "Stone", "Dirt" };
+						digChoice = JOptionPane.showOptionDialog(frame,
+								"You approach a wall of stone and a wall of dirt. Which one do you did through?",
+								"Your Choice", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null,
+								options1, null);
 
-					break;
-				}
-
-				}
+					}
 			}
 
 		public static void thirdBranchResults()
 			{
 				// Scale Branch
 
-				switch (scaleChoice)
-				{
+				if (scaleChoice == 0 && adventureType.equals("Scale the wall"))
+					{
+						//Go down stairs 
+						
+						JOptionPane.showMessageDialog(frame, "You go down the stairs");
+						JOptionPane.showMessageDialog(frame, "Your get caught by the guards");
+						System.exit(0);
+					}
+				else if (scaleChoice == 1 && adventureType.equals("Scale the wall"))
+					{
+						// Go down rope (Win condition)
 
-				case 0:
-				{
-					JOptionPane.showMessageDialog(frame, "You go down the stairs");
-					JOptionPane.showMessageDialog(frame, "Your get caught by the guards");
-					System.exit(0);
-					
-					break;
-				}
-				case 1:
-				{
+						JOptionPane.showMessageDialog(frame, "You choose to go down the rope");
+						JOptionPane.showMessageDialog(frame, "You find the treasure");
 
-					// Go down rope (Win condition)
+						// Random Squander Generator
 
-					JOptionPane.showMessageDialog(frame, "You chose to go down the rope");
-					JOptionPane.showMessageDialog(frame, "You find the treasure");
+						int randomNumberOfTreasure = (int) (Math.random() * 500 + 1);
 
-					// Random Squander Generator
-
-					int randomNumberOfTreasure = (int) (Math.random() * 500 + 1);
-
-					JOptionPane.showMessageDialog(frame, "You get " + randomNumberOfTreasure + " dabloons");
-					int randomNumber1 = (int) (Math.random() * 2);
-					if (randomNumber1 == 0)
-						{
-							JOptionPane.showMessageDialog(frame, "You squander your wealth");
-						}
-					else
-						{
-							JOptionPane.showMessageDialog(frame, "You are smart with your money and will die wealthy");
-						}
-					System.exit(0);
-					break;
-				}
-
-				}
+						JOptionPane.showMessageDialog(frame, "You get " + randomNumberOfTreasure + " dabloons");
+						int randomNumber1 = (int) (Math.random() * 2);
+						if (randomNumber1 == 0)
+							{
+								JOptionPane.showMessageDialog(frame, "You squander your wealth");
+							}
+						else
+							{
+								JOptionPane.showMessageDialog(frame,
+										"You are smart with your money and will die wealthy");
+							}
+						System.exit(0);
+					}
+				
 
 				// dig Branch
 
-				switch (digChoice)
-				{
-				case 0:
-				{
+				if (digChoice == 0 && adventureType.equals("Dig under the wall"))
+					{
+						// Dig Through Stone (Lose Condition)
 
-					// Dig Through Stone (Lose Condition)
+						JOptionPane.showMessageDialog(frame, "You choose to dig through the stone");
+						JOptionPane.showMessageDialog(frame, "Your shovel breaks, you are stuck");
+						System.exit(0);
+					}
+				else if (digChoice == 1 && adventureType.equals("Dig under the wall"))
+					{
+						// Dig Through Dirt (Win Condition)
 
-					JOptionPane.showMessageDialog(frame, "You chose to dig through the stone");
-					JOptionPane.showMessageDialog(frame, "Your shovel breaks, you are stuck");
-					System.exit(0);
-					break;
-				}
-				case 1:
-				{
+						JOptionPane.showMessageDialog(frame, "You dig through the dirt");
+						JOptionPane.showMessageDialog(frame, "You find the treasure");
 
-					// Dig Through Dirt (Win Condition)
+						// Random Squander Money
 
-					JOptionPane.showMessageDialog(frame, "You find the treasure");
+						int randomNumberOfTreasure = (int) (Math.random() * 500 + 1);
 
-					// Random Squander Money
+						JOptionPane.showMessageDialog(frame, "You get " + randomNumberOfTreasure + " dabloons");
 
-					int randomNumberOfTreasure = (int) (Math.random() * 500 + 1);
-
-					JOptionPane.showMessageDialog(frame, "You get " + randomNumberOfTreasure + " dabloons");
-
-					int randomNumber2 = (int) (Math.random() * 2);
-					if (randomNumber2 == 0)
-						{
-							JOptionPane.showMessageDialog(frame, "You squander your wealth");
-						}
-					else if (randomNumber2 == 1)
-						{
-							JOptionPane.showMessageDialog(frame, "You are smart with your money and die wealthy");
-						}
-					System.exit(0);
-					break;
-
-				}
-				}
+						int randomNumber2 = (int) (Math.random() * 2);
+						if (randomNumber2 == 0)
+							{
+								JOptionPane.showMessageDialog(frame, "You squander your wealth");
+							}
+						else if (randomNumber2 == 1)
+							{
+								JOptionPane.showMessageDialog(frame,
+										"You are smart with your money and die wealthy");
+							}
+						System.exit(0);
+					}
+				
 
 			}
 	}
